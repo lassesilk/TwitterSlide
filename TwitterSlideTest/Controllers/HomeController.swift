8c7659841801f07fb393c4f42ba7c5567f22b11b
Lasse N Silkoset <lassesilkoset@gmail.com>
2018-12-07 22:01:47 +0100

    Circular navBar button

--- TwitterSlideTest/Controllers/HomeController.swift
+++ TwitterSlideTest/Controllers/HomeController.swift
@@ -19,138 +19,50 @@ class HomeController: UITableViewController, UIGestureRecognizerDelegate {
         
         tableView.backgroundColor = .red
         setupNavigationItems()
-        setupMenuController()
-//        setupPanGesture()
-        setupDarkCoverView()
-    }
-    
-    let darkCoverView = UIView()
-    
-    fileprivate func setupDarkCoverView() {
-        darkCoverView.alpha = 0
-        darkCoverView.backgroundColor = UIColor(white: 0, alpha: 0.8)
-        darkCoverView.isUserInteractionEnabled = false
-        
-        let mainWindow = UIApplication.shared.keyWindow
-        mainWindow?.addSubview(darkCoverView)
-        darkCoverView.frame = mainWindow?.frame ?? .zero
-    }
-    
-    fileprivate func setupPanGesture() {
-        let panGesture = UIPanGestureRecognizer(target: self, action: #selector(handlePan))
-        panGesture.delegate = self
-        view.addGestureRecognizer(panGesture)
-    }
-    
-//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-//        return true
-//    }
-    
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        menuController.view.frame = CGRect(x: 0, y: 0, width: 200, height: 500)
-    }
-    
-    @objc func handlePan(gesture: UIPanGestureRecognizer) {
-     
-        let translation = gesture.translation(in: self.view)
-        
-        if gesture.state == .changed {
-            var x = translation.x
-            
-            if isMenuOpened {
-                // So HomeController can slide in back in place of MenuController
-                x += menuWidth
-            }
-            
-            x = min(menuWidth, x)
-            x = max(0, x)
-            
-            let transform = CGAffineTransform(translationX: x, y: 0)
-            menuController.view.transform = transform
-            navigationController?.view.transform = transform
-            darkCoverView.transform = transform
-            
-            let alpha = x / menuWidth
-            darkCoverView.alpha = alpha
-            
-        } else if gesture.state == .ended {
-            handleEnded(gesture: gesture)
-        }
-    }
-    
-    fileprivate func handleEnded(gesture: UIPanGestureRecognizer) {
-        let translation = gesture.translation(in: self.view)
-        
-        let velocity = gesture.velocity(in: self.view)
-        
-        if isMenuOpened {
-            if abs(velocity.x) > velocityOpenThreshold {
-                handleHide()
-                return
-            }
-            if abs(translation.x) < menuWidth / 2 {
-                handleOpen()
-            } else {
-                handleHide()
-            }
-        } else {
-            if velocity.x > velocityOpenThreshold {
-                handleOpen()
-                return
-            }
-        if translation.x < menuWidth / 2 {
-            handleHide()
-        } else {
-            handleOpen()
-            }
-        }
-    }
-    
-    let menuController = MenuController()
-    
-    fileprivate let velocityOpenThreshold: CGFloat = 500
-    fileprivate let menuWidth: CGFloat = 300
-    fileprivate var isMenuOpened = false
-    
-    fileprivate func preformAnimations(transform: CGAffineTransform) {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            self.menuController.view.transform = transform
-//            self.view.transform = transform
-            self.navigationController?.view.transform = transform
-            self.darkCoverView.transform = transform
-            
-//            ternary operator, same as if else under
-//            self.darkCoverView.alpha = transform == .identity ? 0 : 1
-            
-            if transform == .identity {
-            self.darkCoverView.alpha = 0
-            } else {
-                self.darkCoverView.alpha = 1
-            }
-        })
     }
     
     @objc func handleOpen() {
-        isMenuOpened = true
-        preformAnimations(transform: CGAffineTransform(translationX: self.menuWidth, y: 0))
+        (UIApplication.shared.keyWindow?.rootViewController as? BaseSlidingController)?.openMenu()
     }
     
     @objc func handleHide() {
-        isMenuOpened = false
-        preformAnimations(transform: .identity)
+       (UIApplication.shared.keyWindow?.rootViewController as? BaseSlidingController)?.closeMenu()
     }
     
-    fileprivate func setupMenuController() {
-        menuController.view.frame = CGRect(x: -menuWidth, y: 0, width: menuWidth, height: self.view.frame.height)
-        let mainWindow = UIApplication.shared.keyWindow
-        mainWindow?.addSubview(menuController.view)
-        addChild(menuController)
-    }
+   
     
     fileprivate func setupNavigationItems() {
         navigationItem.title = "Home"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        
+        setupCircularNavigationButton()
+        
+//        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(handleOpen))
+        
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Hide", style: .plain, target: self, action: #selector(handleHide))
+    }
+    
+    fileprivate func setupCircularNavigationButton() {
+        let image = #imageLiteral(resourceName: "Lasse").withRenderingMode(.alwaysOriginal)
+        
+        let customView = UIButton(type: .system)
+        customView.addTarget(self, action: #selector(handleOpen), for: .touchUpInside)
+        customView.setImage(image, for: .normal)
+        customView.imageView?.contentMode = .scaleAspectFill
+        
+        customView.layer.cornerRadius = 20
+        customView.clipsToBounds = true
+        
+        //customView uses autoLayout to put itself in navBar. Not respecting this frame
+//        customView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        
+        //Use constraints to create own layout
+        customView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        customView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        let barButtonItem = UIBarButtonItem(customView: customView)
+        
+        navigationItem.leftBarButtonItem = barButtonItem
+        
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -164,7 +76,5 @@ class HomeController: UITableViewController, UIGestureRecognizerDelegate {
         
         return cell
     }
-
-
 }
 
